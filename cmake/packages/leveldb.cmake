c906e8471759d12c792506edbe3bdfc8da475ef6
lltcggie <lltcggie@gmail.com>
2015-11-29 12:58:22 +0900

    Update leveldb

--- cmake/packages/leveldb.cmake
+++ cmake/packages/leveldb.cmake
@@ -2,7 +2,7 @@ include(buildem_status)
 include(buildem_download_package)
 include(buildem_cmake_recipe)
 
-buildem_download_package(GIT_REPOSITORY "https://github.com/willyd/leveldb.git"
+buildem_download_package(GIT_REPOSITORY "https://github.com/lltcggie/leveldb.git"
                          GIT_TAG cmake
 						 SOURCE_DIR leveldb_SOURCE_DIR)
 
