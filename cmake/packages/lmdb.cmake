ea60f07a214c370b610ab9175ffaffa9995289a7
lltcggie <lltcggie@gmail.com>
2015-12-01 11:03:31 +0900

    Add USE_STATIC_RUNTIME_LINK option

--- cmake/packages/lmdb.cmake
+++ cmake/packages/lmdb.cmake
@@ -2,13 +2,14 @@ include(buildem_status)
 include(buildem_download_package)
 include(buildem_cmake_recipe)
 
-buildem_download_package(GIT_REPOSITORY "https://github.com/willyd/lmdb.git"
+buildem_download_package(GIT_REPOSITORY "https://github.com/lltcggie/lmdb.git"
                          GIT_BRANCH cmake
 						 SOURCE_DIR lmdb_SOURCE_DIR)
 
 set(lmdb_CMAKE_ARGS 
 	-DBUILD_SHARED_LIBS=OFF
 	-DCMAKE_DEBUG_POSTFIX=d
+	-DBUILD_WITH_STATIC_CRT=${USE_STATIC_RUNTIME_LINK}
 	)
 
 buildem_cmake_recipe(NAME lmdb  
